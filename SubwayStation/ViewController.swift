c5d11ca7fd78f65119ce8e5625b37941a2ab90e2
hemg2 <hemg2@naver.com>
2023-01-02 14:22:12 +0900

    SceneDelegate

---
+++ SubwayStation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SubwayStation
+//
+//  Created by 1 on 2023/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+
+
+}
